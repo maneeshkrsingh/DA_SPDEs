1465fedafa0756ff1aac4fc6e572af31c7b1292d
maneeshkrsingh <maneesh.sr51@gmail.com>
2023-09-19 10:28:46 +0100

    2nd order dw_solver

--- stochastic_CH/dw_solver.py
+++ stochastic_CH/dw_solver.py
@@ -9,18 +9,23 @@ x, = SpatialCoordinate(mesh)
 V = FunctionSpace(mesh, "CG", 1)
 W_F = FunctionSpace(mesh, "DG", 0) 
 dW = Function(W_F) 
+dM = Function(V) 
 
 alpha_w = CellVolume(mesh)
 
 dphi = TestFunction(V)
 du = TrialFunction(V)
+dm = TrialFunction(V)
         
 # dU = Function(V)
 kappa_isq = 0.01
-a_w = (dphi*du + kappa_isq*dphi.dx(0)*du.dx(0))*dx
+a_u = (dphi*du + kappa_isq*dphi.dx(0)*du.dx(0))*dx
+a_m =  (dphi*dm + kappa_isq*dphi.dx(0)*dm.dx(0))*dx
+
 sp={'ksp_type': 'preonly', 'pc_type': 'lu'}
 
 du = Function(V)
+dm = Function(V)
 
 step = 0
 ufile = File('dW_fig/du.pvd')
@@ -35,7 +40,9 @@ for step in range(nstep):
     rg = RandomGenerator(pcg)
     dW.assign(rg.normal(W_F, 0., 0.125))
     L_w = alpha_w*dphi*dW*dx
-    solve(a_w == L_w, du, solver_parameters=sp)
+    solve(a_m == L_w, dm, solver_parameters=sp)
+    L_m = dphi*dm*dx
+    solve(a_u == L_m, du, solver_parameters=sp)
     du.assign(du)
     print('dW', dW.dat.data)
     print('dU', du.dat.data)
