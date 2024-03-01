114bbdd04fac267f4e5749e62d2356290cf17d4d
maneeshkrsingh <maneesh.sr51@gmail.com>
2024-03-01 12:08:00 +0000

    tidying up

--- checkpoint_parallel_test/load_checkpoint_parallel.py
+++ checkpoint_parallel_test/load_checkpoint_parallel.py
@@ -1,11 +1,8 @@
 from firedrake import *
 from nudging import *
-
-
 from nudging import LSDEModel
 
-
-T = 1.
+T =1 
 nsteps = 10
 dt = T/nsteps
 A = 1.
@@ -22,8 +19,10 @@ with CheckpointFile("example.h5",  'r') as afile:
     mesh = afile.load_mesh("meshA")
     for ilocal in range(nensemble[simfilter.ensemble_rank]):
         iglobal = simfilter.layout.transform_index(ilocal, itype='l', rtype='g')
-        f = afile.load_function(mesh, "f", idx = iglobal)
+        print('ilocal', ilocal, 'iglobal', iglobal)
+        f = afile.load_function(mesh, "f", idx = ilocal)
+        print('norm of f', norm(f))
         q = simfilter.ensemble[ilocal][0]
         q.interpolate(f)
-        print('ilocal', ilocal, 'iglobal', iglobal, norm(q))
+        #print('ilocal', ilocal, 'iglobal', iglobal, norm(q))
 
--- checkpoint_parallel_test/save_checkpoint.py
+++ checkpoint_parallel_test/save_checkpoint.py
@@ -1,12 +1,10 @@
-from firedrake import *
 import firedrake as fd
-from nudging import *
-
 
 
 nensemble = [5]*5
 
 mesh = fd.UnitIntervalMesh(1,  name="meshA")
+
 V = fd.FunctionSpace(mesh, "CG", 1)
 f = fd.Function(V, name="f")
 
@@ -14,5 +12,6 @@ with fd.CheckpointFile("example.h5", 'w') as afile:
     afile.save_mesh(mesh) 
     for i in range(sum(nensemble)):
         q = fd.Constant(1.0)
-        f.interpolate(q)
+        f.interpolate(q*i)
+        print(fd.norm(f))
         afile.save_function(f, idx = i)
