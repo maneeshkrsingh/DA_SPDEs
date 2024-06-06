bbb15484575ebc51d87f096600e352c69d72e9bc
maneeshkrsingh <maneesh.sr51@gmail.com>
2024-06-06 12:01:42 +0100

    tidying up checkoint DA

--- stochastic_euler/checkpoint_assimilate_data.py
+++ stochastic_euler/checkpoint_assimilate_data.py
@@ -1,68 +1,52 @@
 from firedrake import *
-from firedrake.__future__ import interpolate
 from nudging import *
 import numpy as np
-import matplotlib.pyplot as plt
 import gc
 import petsc4py
 from firedrake.petsc import PETSc
-from pyop2.mpi import MPI
-from pyadjoint import AdjFloat
-
-
-
 
 from nudging.models.stochastic_euler import Euler_SD
 import os
 
 os.makedirs('../../DA_Results/2DEuler/', exist_ok=True)
-
-""" read obs from saved file 
-    Do assimilation step for tempering and jittering steps 
+""" read obs from saved file Do assimilation step for tempering and jittering steps 
 """
-
 # Load data
 u_exact = np.load('../../DA_Results/2DEuler/u_true_data_par.npy')
 u_vel = np.load('../../DA_Results/2DEuler/u_obs_data_par.npy') 
 
-nensemble = [6]*25
-
-
-# N_obs = 500
-n = 64
+nensemble = [4]*25
+n = 32
 nsteps = 5
 dt = 0.025
 
-model = Euler_SD(n, nsteps=nsteps, mesh = False, dt = dt, noise_scale=0.25, lambdas=True, salt=True)
+model = Euler_SD(n, nsteps=nsteps, mesh = False, dt = dt,  noise_scale=0.025, salt=True,  lambdas=True)
 
-
-MALA = False
-verbose = True
+#MALA = False
+#verbose = True
 nudging = False
 jtfilter = jittertemp_filter(n_jitt = 5, delta = 0.15,
-                              verbose=verbose, MALA=MALA,
-                              nudging=nudging, visualise_tape=False)
-# jtfilter = bootstrap_filter(verbose=verbose)
+                              verbose=2, MALA=False,
+                              nudging=nudging)
+# jtfilter = bootstrap_filter(verbose=2)
 
-jtfilter.setup(nensemble, model)
-
+jtfilter.setup(nensemble=nensemble, model=model, residual=False)
 
 with CheckpointFile("../../DA_Results/2DEuler/checkpoint_files/ensemble_init.h5", 
                        'r', comm=jtfilter.subcommunicators.comm) as afile:
     mesh = afile.load_mesh("mesh2d_per")
     for ilocal in range(nensemble[jtfilter.ensemble_rank]):
         iglobal = jtfilter.layout.transform_index(ilocal, itype='l', rtype='g')
-        f = afile.load_function(mesh, "f_chp", idx = iglobal) # checkpoint at only ranks 
+        f = afile.load_function(mesh, "f_chp", idx = iglobal) # checkpoint 
         q = jtfilter.ensemble[ilocal][0]
         q.interpolate(f)
         #print('ilocal', ilocal, 'iglobal', iglobal, norm(q))
 
-
+SD = 0.005
 
 def log_likelihood(y, Y):
-    ll = (y-Y)**2/0.1**2/2*dx
+    ll =(y-Y)**2/SD**2/2*dx
     return ll
-
 
 N_obs = u_vel.shape[0]
 # VVOM Function
@@ -70,34 +54,46 @@ u_VOM = Function(model.VVOM)
 # prepare shared arrays for data
 u1_e_list = []
 u2_e_list = []
-u1_sim_list = []
-u2_sim_list = []
 
 for m in range(u_vel.shape[1]):        
     u1_e_shared = SharedArray(partition=nensemble, 
                                  comm=jtfilter.subcommunicators.ensemble_comm)
     u2_e_shared = SharedArray(partition=nensemble, 
                                  comm=jtfilter.subcommunicators.ensemble_comm)
-    u1_sim_shared = SharedArray(partition=nensemble, 
-                                 comm=jtfilter.subcommunicators.ensemble_comm)
-    u2_sim_shared = SharedArray(partition=nensemble, 
-                                 comm=jtfilter.subcommunicators.ensemble_comm)
-  
+    
     u1_e_list.append(u1_e_shared)
     u2_e_list.append(u2_e_shared)
-    u1_sim_list.append(u1_sim_shared)
-    u2_sim_list.append(u2_sim_shared)
-  
-
-
+    
 ushape = u_vel.shape
 if COMM_WORLD.rank == 0:
     u1_e = np.zeros((np.sum(nensemble), ushape[0], ushape[1]))
     u2_e = np.zeros((np.sum(nensemble), ushape[0], ushape[1]))
-    u1_sim_obs_alltime_step = np.zeros((np.sum(nensemble), nsteps, ushape[1]))
-    u2_sim_obs_alltime_step = np.zeros((np.sum(nensemble), nsteps, ushape[1]))
-    u1_sim_obs_allobs_step = np.zeros((np.sum(nensemble), nsteps*ushape[0], ushape[1]))
-    u2_sim_obs_allobs_step = np.zeros((np.sum(nensemble), nsteps*ushape[0], ushape[1]))
+
+#################################
+### digonstic details
+# results in a diagnostic
+class samples(base_diagnostic):
+    def compute_diagnostic(self, particle):
+        model.q0.assign(particle[0])
+        return model.obs().dat.data[0]
+
+
+resamplingsamples = samples(Stage.AFTER_ASSIMILATION_STEP,
+                            jtfilter.subcommunicators,
+                            nensemble)
+nudgingsamples = samples(Stage.AFTER_NUDGING,
+                         jtfilter.subcommunicators,
+                         nensemble)
+nolambdasamples = samples(Stage.WITHOUT_LAMBDAS,
+                          jtfilter.subcommunicators,
+                          nensemble)
+
+diagnostics = [nudgingsamples,
+               resamplingsamples,
+               nolambdasamples]
+
+
+
 
 # do assimiliation step
 for k in range(N_obs):
@@ -106,28 +102,11 @@ for k in range(N_obs):
     u_VOM.dat.data[:,0] = u_vel[k,:,0]
     u_VOM.dat.data[:,1] = u_vel[k,:,1]
 
+    # jtfilter.assimilation_step(u_VOM, log_likelihood)
 
-    # print simulated data for velocity
-    for step in range(nsteps):
-        for i in range(nensemble[jtfilter.ensemble_rank]):
-            model.run(jtfilter.ensemble[i], jtfilter.ensemble[i])
-            fwd_simdata1 = model.obs().dat.data[:][:,0]
-            fwd_simdata2 = model.obs().dat.data[:][:,1]
-            for m in range(u_vel.shape[1]):
-                u1_sim_list[m].dlocal[i] = fwd_simdata1[m]
-                u2_sim_list[m].dlocal[i] = fwd_simdata2[m]
-        # store all timesteps values
-        for m in range(u_vel.shape[1]):
-            u1_sim_list[m].synchronise()
-            u2_sim_list[m].synchronise()
-            if COMM_WORLD.rank == 0:
-                u1_sim_obs_alltime_step[:, step, m] = u1_sim_list[m].data()
-                u1_sim_obs_allobs_step[:,nsteps*k+step,m] = u1_sim_obs_alltime_step[:, step, m]
-                u2_sim_obs_alltime_step[:, step, m] = u2_sim_list[m].data()
-                u2_sim_obs_allobs_step[:,nsteps*k+step,m] = u2_sim_obs_alltime_step[:, step, m]
-
-    jtfilter.assimilation_step(u_VOM, log_likelihood)
-
+    jtfilter.assimilation_step(u_VOM, log_likelihood,
+                           ess_tol=0.8)
+    # garbage cleanup --not sure if improved speed
     PETSc.garbage_cleanup(PETSc.COMM_SELF)
     petsc4py.PETSc.garbage_cleanup(model.mesh._comm)
     petsc4py.PETSc.garbage_cleanup(model.mesh.comm)
@@ -144,21 +123,15 @@ for k in range(N_obs):
     for m in range(u_vel.shape[1]):
         u1_e_list[m].synchronise()
         u2_e_list[m].synchronise()
-
         if COMM_WORLD.rank == 0:
             u1_e[:, k, m] = u1_e_list[m].data()
             u2_e[:, k, m] = u2_e_list[m].data()
             
-
-#PETSc.Sys.Print("--- %s seconds ---" % (time.time() - start_time))
+# #PETSc.Sys.Print("--- %s seconds ---" % (time.time() - start_time))
 if COMM_WORLD.rank == 0:
     u_e = np.stack((u1_e,u2_e), axis = -1)
-    u_sim_allobs_step = np.stack(( u1_sim_obs_allobs_step, u2_sim_obs_allobs_step), axis = -1)
     print(u_e.shape)
-    #print(u_sim_allobs_step.shape)
-
     if not nudging:
-        np.save("../../DA_Results/2DEuler/mcmc_assimilated_Velocity_ensemble.npy", u_e)
+        np.save("../../DA_Results/2DEuler/mcmcwt_assimilated_Velocity_ensemble.npy", u_e)
     if nudging:
         np.save("../../DA_Results/2DEuler/nudge_assimilated_Velocity_ensemble.npy", u_e)
-
--- stochastic_euler/checkpoint_generate_data.py
+++ stochastic_euler/checkpoint_generate_data.py
@@ -17,15 +17,15 @@ run model, get true value and obseravation and use paraview for viewing
 add observation noise N(0, sigma^2) 
 """
 
-# truth_init = VTKFile("../../DA_Results/2DEuler/paraview_adtnoise/truth_init.pvd")
-# truth = VTKFile("../../DA_Results/2DEuler/paraview_adtnoise/truth.pvd")
-# truth_init_ptb = VTKFile("../../DA_Results/2DEuler/paraview_adtnoise/truth_init_ptb.pvd")
-# particle_init = VTKFile("../../DA_Results/2DEuler/paraview_adtnoise/particle_init.pvd")
-#np.random.seed(138)
-nensemble = [1]*1
+truth_init = VTKFile("../../DA_Results/2DEuler/paraview_saltadtnoise/truth_init.pvd")
+truth = VTKFile("../../DA_Results/2DEuler/paraview_saltadtnoise/truth.pvd")
+truth_init_ptb = VTKFile("../../DA_Results/2DEuler/paraview_saltadtnoise/truth_init_ptb.pvd")
+particle_init = VTKFile("../../DA_Results/2DEuler/paraview_saltadtnoise/particle_init.pvd")
+
+nensemble = [10]*1
 N_obs = 1
-N_init = 5
-n = 64
+N_init = 1000
+n = 32
 nsteps = 5
 dt = 0.0125
 
@@ -33,7 +33,9 @@ dt = 0.0125
 comm=fd.COMM_WORLD
 mesh = fd.UnitSquareMesh(n, n, quadrilateral = True, comm=comm, name ="mesh2d_per")
 
-model = Euler_SD(n, nsteps=nsteps, mesh = mesh, dt = dt, noise_scale=0.5, salt=False)
+#model = Euler_SD(n, nsteps=nsteps, mesh = mesh, dt = dt, noise_scale=0.025)
+model = Euler_SD(n, nsteps=nsteps, mesh = mesh, dt = dt, noise_scale=0.025, salt=True,  lambdas=True)
+
 
 model.setup()
 
@@ -49,7 +51,8 @@ v = Function(Vu, name="gradperp(stream function)")
 ############################# initilisation ############
 X0_truth = model.allocate()
 
-X0_truth[0].interpolate(sin(8*pi*x[0])*sin(8*pi*x[1])+0.4*cos(6*pi*x[0])*cos(6*pi*x[1]))
+X0_truth[0].interpolate(sin(8*pi*x[0])*sin(8*pi*x[1])+0.4*cos(6*pi*x[0])*cos(6*pi*x[1])
+                        +02.3*cos(10*pi*x[0])*cos(4*pi*x[1]) +0.02*sin(2*pi*x[0])+ 0.02*sin(2*pi*x[1]))
 
 
 # run model for 100 (20*5)  times and store inital vorticity for generating data
@@ -68,13 +71,18 @@ u1_init = np.zeros((np.size(v1_true)))
 u2_init = np.zeros((np.size(v2_true)))
 
 for i in range(N_init):
-    PETSc.Sys.Print('=================init_step====================', i)
+    PETSc.Sys.Print('========init_step=============', i)
     model.randomize(X0_truth)
     model.run(X0_truth, X0_truth)
 
     model.q1.assign(X0_truth[0])
     model.psi_solver.solve()  # solved at t+1 for psi
-
+    v.project(model.gradperp(model.psi0))
+    truth_init.write(model.q1, model.psi0, v)
+    # model.phi_fn.project(dt*model.psi0)
+    # model.phi_mod_fn.project(dt*model.psi0+dt*model.noise_scale*model.dU_3*dt**0.5)
+    # print('phi', np.abs(model.phi_fn.dat.data[:]).max())
+    # print('phi_mod', np.abs(model.phi_mod_fn.dat.data[:]).max())
     u_init_VOM = model.obs()
     u_init_VOM_out = Function(model.VVOM_out)
     u_init_VOM_out.interpolate(u_init_VOM)
@@ -90,43 +98,25 @@ for i in range(N_init):
 X_truth = model.allocate()
 X_truth[0].assign(X0_truth[0])
 
-model.q1.assign(X_truth[0])
-model.psi_solver.solve()  # solved at t+1 for psi
-v.project(model.gradperp(model.psi0))
-#truth_init.write(model.q1, model.psi0, v)
+# model.q1.assign(X_truth[0])
+# model.psi_solver.solve()  # solved at t+1 for psi
+# v.project(model.gradperp(model.psi0))
+# #print(type(v))
+# truth_init.write(model.q1, model.psi0, v)
+
+####################################### compute Courant number ###################
 
 
-#print(norm(X_truth[0]))
-##########################################################################
-
-
-# # #To store velocity values 
-
-# u_VOM = model.obs()
-# u_VOM_out = Function(model.VVOM_out)
-# u_VOM_out.interpolate(u_VOM)
-# u = u_VOM_out.dat.data_ro.copy()
-
-# v_true = u_VOM_out.dat.data_ro.copy()
-
-# v1_true = v_true[:,0]
-# v2_true = v_true[:,1]
 
 u1_true_all = np.zeros((N_obs, np.size(v1_true)))
 u2_true_all = np.zeros((N_obs, np.size(v2_true)))
 u1_obs_all = np.zeros((N_obs, np.size(v1_true)))
 u2_obs_all = np.zeros((N_obs, np.size(v2_true)))
 
-
-# model.q1.rename("Potential vorticity")
-# model.psi0.rename("Stream function")
-# Vu = VectorFunctionSpace(mesh, "DQ", 0)  # DQ elements for velocity
-# v = Function(Vu, name="gradperp(stream function)")
-
-
-#truth.write(model.q1, model.psi0, v)
 u_energy = []
-# Exact numerical approximation 
+
+# Forwrad run for truth
+
 for i in range(N_obs):
     PETSc.Sys.Print('=============In N_obs step=================', i)
     model.randomize(X_truth)
@@ -136,7 +126,7 @@ for i in range(N_obs):
     v.project(model.gradperp(model.psi0))
     #print(norm(v))
     u_energy.append(norm(v))
-    #truth.write(model.q1, model.psi0, v)
+    truth.write(model.q1, model.psi0, v)
 
     u_VOM = model.obs()
     u_VOM_out = Function(model.VVOM_out)
@@ -147,11 +137,9 @@ for i in range(N_obs):
         u1_true_all[i,:] = u[:,0]
         u2_true_all[i,:] = u[:,1]
 
-
-    
-        u_1_noise = np.random.normal(0.0, 0.1, (n+1)**2 ) # mean = 0, sd = 0.05
-        u_2_noise = np.random.normal(0.0, 0.1, (n+1)**2 ) 
-
+        u_1_noise = np.random.normal(0.0, 0.005, (n+1)**2 ) # mean = 0, sd = 0.05
+        u_2_noise = np.random.normal(0.0, 0.005, (n+1)**2 ) 
+        print(u_1_noise.max(), u_2_noise.min())
         u1_obs = u[:,0] + u_1_noise
         u2_obs = u[:,1] + u_2_noise
 
@@ -164,75 +152,118 @@ if comm.rank == 0:
     u_obs_all = np.stack((u1_obs_all, u2_obs_all), axis=-1)
 
 
-    # geu_Energy = np.array((u_energy))
-    #np.save("../../DA_Results/2DEuler/u_true_data_par.npy", u_true_all)
-    #np.save("../../DA_Results/2DEuler/u_obs_data_par.npy", u_obs_all)
+    np.save("../../DA_Results/2DEuler/u_true_data_par.npy", u_true_all)
+    np.save("../../DA_Results/2DEuler/u_obs_data_par.npy", u_obs_all)
     # np.save("../../DA_Results/2DEuler/u_energy_new.npy", u_Energy)
 
 # u_Energy = np.array((u_energy))
 # np.save("../../DA_Results/2DEuler/u_energy_new.npy", u_Energy)
 
-# # # To create initilaztion of particles, setup checkpointing
-# # # small pertirbation in init
-a = model.rg.normal(model.R, 0., 0.1) 
-b = model.rg.normal(model.R, 0., 0.1)
-# c = model.rg.normal(model.R, 0., 0.1) 
-# d = model.rg.normal(model.R, 0., 0.1)
-
-X0_pertb = model.allocate()
-X0_pertb[0].interpolate((1+a)*sin(8*pi*(x[0]))*sin(8*pi*(x[1]))+0.4*(1+b)*cos(6*pi*(x[0]))*cos(6*pi*(x[1])))
-
-# run model for 100 times and store inital vorticity for generating data
-for i in range(N_init):
-    PETSc.Sys.Print('==============In pertb step====================', i)
-    model.randomize(X0_pertb)
-    model.run(X0_pertb, X0_pertb)
-
-X_new = model.allocate()
-X_new[0].assign(X0_pertb[0])
-model.q1.assign(X_new[0])
-model.psi_solver.solve()  # solved at t+1 for psi
-v.project(model.gradperp(model.psi0))
-#truth_init_ptb.write(model.q1, model.psi0, v)
-
-
-
+################### same initialization as truth
 # # #now do checkpointing
-Vdg = fd.FunctionSpace(mesh, "DQ", 1)  # PV space
-f_chp = Function(Vdg, name="f_chp")
+# Vdg = fd.FunctionSpace(mesh, "DQ", 1)  # PV space
+# f_chp = Function(Vdg, name="f_chp")
+# #dump data 
+# ndump = 5
+# p_dump = 0
 
+# u1_particle_init = np.zeros((sum(nensemble), np.size(v1_true)))
+# u2_particle_init = np.zeros((sum(nensemble), np.size(v2_true)))
+# with fd.CheckpointFile("../../DA_Results/2DEuler/checkpoint_files/ensemble_init.h5", 
+#                        'w') as afile:
+#     afile.save_mesh(mesh)
+#     for i in range(sum(nensemble)*ndump):
+#         model.randomize(X_truth)
+#         model.run(X_truth, X_truth)
+#         if i % ndump == 0:
+#             PETSc.Sys.Print('===========checkpoint particle============', p_dump , 'init')
+#             f_chp.interpolate(X_truth[0])
+#             model.q1.assign(X_truth[0])
+#             model.psi_solver.solve()  # solved at t+1 for psi
+#             v.project(model.gradperp(model.psi0))
+#             particle_init.write(model.q1, model.psi0, v)
+#             afile.save_function(f_chp, idx=p_dump)
+#             u_particle_VOM = model.obs()
+#             u_particle_VOM_out = Function(model.VVOM_out)
+#             u_particle_VOM_out.interpolate(u_particle_VOM)
+#             u_particle = u_particle_VOM_out.dat.data_ro.copy()
 
-#dump data 
-ndump = 2
-p_dump = 0
-
-
-u1_particle_init = np.zeros((sum(nensemble), np.size(v1_true)))
-u2_particle_init = np.zeros((sum(nensemble), np.size(v2_true)))
-with fd.CheckpointFile("../../DA_Results/2DEuler/checkpoint_files/ensemble_init.h5", 
-                       'w') as afile:
-    afile.save_mesh(mesh)
-    for i in range(sum(nensemble)*ndump):
-        model.randomize(X_new)
-        model.run(X_new, X_new)
-        if i % ndump == 0:
-            PETSc.Sys.Print('===========checkpoint particle============', p_dump , 'init')
-            f_chp.interpolate(X_new[0])
-            model.q1.assign(X_new[0])
-            model.psi_solver.solve()  # solved at t+1 for psi
-            v.project(model.gradperp(model.psi0))
-            #particle_init.write(model.q1, model.psi0, v)
-            afile.save_function(f_chp, idx=p_dump)
-            u_particle_VOM = model.obs()
-            u_particle_VOM_out = Function(model.VVOM_out)
-            u_particle_VOM_out.interpolate(u_particle_VOM)
-            u_particle = u_particle_VOM_out.dat.data_ro.copy()
-
-            if comm.rank == 0:
-                u1_particle_init[p_dump,:] = u_particle[:,0]
-                u2_particle_init[p_dump,:] = u_particle[:,1]
-                #print(u1_particle_init)
-                u_particle_init = np.stack((u1_particle_init, u2_particle_init), axis=-1)
-                #np.save("../../DA_Results/2DEuler/u_particle_init.npy", u_particle_init)
+#             if comm.rank == 0:
+#                 u1_particle_init[p_dump,:] = u_particle[:,0]
+#                 u2_particle_init[p_dump,:] = u_particle[:,1]
+#                 #print(u1_particle_init)
+#                 u_particle_init = np.stack((u1_particle_init, u2_particle_init), axis=-1)
+#                 #np.save("../../DA_Results/2DEuler/u_particle_init.npy", u_particle_init)
     
-            p_dump += 1
+#             p_dump += 1
+
+
+
+
+############################## pertubed initlization###################################
+
+# # # # To create initilaztion of particles, setup checkpointing
+# # # # small pertirbation in init
+# a = model.rg.normal(model.R, 0., 0.01) 
+# b = model.rg.normal(model.R, 0., 0.01)
+# # c = model.rg.normal(model.R, 0., 0.1) 
+# # d = model.rg.normal(model.R, 0., 0.1)
+
+# X0_pertb = model.allocate()
+# X0_pertb[0].interpolate((1+a)*sin(8*pi*(x[0]))*sin(8*pi*(x[1]))+0.4*(1+b)*cos(6*pi*(x[0]))*cos(6*pi*(x[1])))
+
+# # run model for 100 times and store inital vorticity for generating data
+# for i in range(N_init):
+#     PETSc.Sys.Print('==============In pertb step====================', i)
+#     model.randomize(X0_pertb)
+#     model.run(X0_pertb, X0_pertb)
+#     model.q1.assign(X0_pertb[0])
+#     model.psi_solver.solve()  # solved at t+1 for psi
+#     v.project(model.gradperp(model.psi0))
+#     truth_init_ptb.write(model.q1, model.psi0, v)
+
+# X_new = model.allocate()
+# X_new[0].assign(X0_pertb[0])
+
+
+
+
+# # # #now do checkpointing
+# Vdg = fd.FunctionSpace(mesh, "DQ", 1)  # PV space
+# f_chp = Function(Vdg, name="f_chp")
+
+
+# #dump data 
+# ndump = 2
+# p_dump = 0
+
+
+# u1_particle_init = np.zeros((sum(nensemble), np.size(v1_true)))
+# u2_particle_init = np.zeros((sum(nensemble), np.size(v2_true)))
+# with fd.CheckpointFile("../../DA_Results/2DEuler/checkpoint_files/ensemble_init.h5", 
+#                        'w') as afile:
+#     afile.save_mesh(mesh)
+#     for i in range(sum(nensemble)*ndump):
+#         model.randomize(X_new)
+#         model.run(X_new, X_new)
+#         if i % ndump == 0:
+#             PETSc.Sys.Print('===========checkpoint particle============', p_dump , 'init')
+#             f_chp.interpolate(X_new[0])
+#             model.q1.assign(X_new[0])
+#             model.psi_solver.solve()  # solved at t+1 for psi
+#             v.project(model.gradperp(model.psi0))
+#             particle_init.write(model.q1, model.psi0, v)
+#             afile.save_function(f_chp, idx=p_dump)
+#             u_particle_VOM = model.obs()
+#             u_particle_VOM_out = Function(model.VVOM_out)
+#             u_particle_VOM_out.interpolate(u_particle_VOM)
+#             u_particle = u_particle_VOM_out.dat.data_ro.copy()
+
+#             if comm.rank == 0:
+#                 u1_particle_init[p_dump,:] = u_particle[:,0]
+#                 u2_particle_init[p_dump,:] = u_particle[:,1]
+#                 #print(u1_particle_init)
+#                 u_particle_init = np.stack((u1_particle_init, u2_particle_init), axis=-1)
+#                 #np.save("../../DA_Results/2DEuler/u_particle_init.npy", u_particle_init)
+    
+#             p_dump += 1
