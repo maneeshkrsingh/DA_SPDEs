c4936aa5d4da821445ec949fcd6fa2dd365415ec
maneeshkrsingh <maneesh.sr51@gmail.com>
2024-06-06 12:02:07 +0100

    fix noise solver

--- stochastic_euler/noise_soln.py
+++ stochastic_euler/noise_soln.py
@@ -4,13 +4,13 @@ from firedrake.output import VTKFile
 from nudging.models.stochastic_euler import Euler_SD
 
 import os
-os.makedirs('../../DA_Results/2DEuler/', exist_ok=True)
+os.makedirs('../../DA_Results/2DEuler/noise_soln/', exist_ok=True)
 
-#noise = VTKFile("../../DA_Results/2DEuler/paraview_noise/noise.pvd")
+noise = VTKFile("../../DA_Results/2DEuler/paraview_noise/noise.pvd")
 
-n= 64
+n= 32
 
-N_time = 100
+N_time = 20
 
 mesh = fd.UnitSquareMesh(n, n, quadrilateral = True)
 model = Euler_SD(n, nsteps=5, mesh = mesh, dt = 0.0125, noise_scale=0.15)
@@ -33,9 +33,9 @@ dU = fd.TrialFunction(Vcg)
 
 cell_area = fd.CellVolume(mesh)
 #alpha_w = (1/cell_area**0.5)
-#kappa_inv_sq = cell_area
-kappa_inv_sq = fd.Constant(1/30**2)
-#print(fd.assemble(kappa_inv_sq*dx(mesh)))
+kappa_inv_sq = fd.Constant(0.0)
+#kappa_inv_sq = fd.Constant(1/30**2)
+print('cell area', (fd.assemble(cell_area*dx(mesh)))**0.5)
 
 dU_1 = fd.Function(Vcg)
 dU_2 = fd.Function(Vcg)
@@ -58,43 +58,64 @@ L_w3 = dU_2*dW_phi*dx
 w_prob3 = fd.LinearVariationalProblem(a_dW, L_w3, dU_3,bcs=bc, constant_jacobian=True)
 wsolver3 = fd.LinearVariationalSolver(w_prob3, solver_parameters=sp)
 
-# for t in range(N_time):
-#     print('step', t)
-#     dW.assign(model.rg.normal(W_F, 0., 1.0))
-#     #print('source noise', fd.assemble(dW*dx))
+for t in range(N_time):
+    print('step', t)
+    dW.assign(model.rg.normal(W_F, 0., 1.0))
+    print('source noise', np.var(dW.dat.data[:]))
 
-#     wsolver1.solve()
-#     wsolver2.solve() # too smooth 
-#     wsolver3.solve()
-#     print('max noise value', np.abs(dU_3.dat.data[:]).max())
-#     #noise.write(dU_3)
+    wsolver1.solve()
+    wsolver2.solve() # too smooth 
+    wsolver3.solve()
+    print('max noise value', np.abs(dU_3.dat.data[:]).max())
+    noise.write(dU_1)
 
-Vdg = fd.FunctionSpace(mesh, "DQ", 1)  # PV space
+# Vdg = fd.FunctionSpace(mesh, "DQ", 1)  # PV space
 
-psi = fd.TrialFunction(Vcg)
-phi = fd.TestFunction(Vcg)
-psi0 = fd.Function(Vcg)
+# psi = fd.TrialFunction(Vcg)
+# phi = fd.TestFunction(Vcg)
+# psi0 = fd.Function(Vcg)
 
-q1 = fd.Function(Vdg)
-q1.interpolate(fd.sin(8*fd.pi*x[0])*fd.sin(8*fd.pi*x[1])+0.4*fd.cos(6*fd.pi*x[0])*fd.cos(6*fd.pi*x[1]))
+# q1 = fd.Function(Vdg)
+# q1.interpolate(fd.sin(8*fd.pi*x[0])*fd.sin(8*fd.pi*x[1])+0.4*fd.cos(6*fd.pi*x[0])*fd.cos(6*fd.pi*x[1]))
 
-# Build the weak form for the inversion
-Apsi = (fd.inner(fd.grad(psi), fd.grad(phi)))*dx
-Lpsi = -q1 * phi * dx
-bc1 = fd.DirichletBC(Vcg, fd.zero(), ("on_boundary"))
+# # Build the weak form for the inversion
+# Apsi = (fd.inner(fd.grad(psi), fd.grad(phi)))*dx
+# Lpsi = -q1 * phi * dx
+# bc1 = fd.DirichletBC(Vcg, fd.zero(), ("on_boundary"))
 
-psi_problem = fd.LinearVariationalProblem(Apsi, Lpsi,
-                                                  psi0,
-                                                  bcs=bc1,
-                                                  constant_jacobian=True)
-sp = {"ksp_type": "cg", "pc_type": "lu",
-              "pc_factor_mat_solver_type": "mumps"}
-psi_solver = fd.LinearVariationalSolver(psi_problem, solver_parameters=sp)
-psi_solver.solve()
-dt = 0.0125
-print (fd.norm(psi0))   # print the infinity norm of vector x
-print(np.abs(psi0.dat.data[:]).max())
-print(np.abs(dt*psi0.dat.data[:]).max())
+# psi_problem = fd.LinearVariationalProblem(Apsi, Lpsi,psi0,bcs=bc1,  constant_jacobian=True)
+# sp = {"ksp_type": "cg", "pc_type": "lu",
+#               "pc_factor_mat_solver_type": "mumps"}
+# psi_solver = fd.LinearVariationalSolver(psi_problem, solver_parameters=sp)
+# psi_solver.solve()
+# dt = 0.0125
+
+# def gradperp(u):
+#     return fd.as_vector((-u.dx(1), u.dx(0)))
+
+# def both(u):
+#     return 2*fd.avg(u)
+
+# n_F = fd.FacetNormal(mesh)
+# DG0 = fd.FunctionSpace(mesh, "DG", 0)
+# One = fd.Function(DG0).assign(1.0)
+# un = 0.5*(fd.inner(-gradperp(psi0), n_F) + abs(fd.inner(-gradperp(psi0), n_F))) # gives fluxes *into* cell only
+# #un = 0.5 * fd.inner(v, n_F)  # gives fluxes *into* cell only#
+# print(type(un))
+# v_courant = fd.TestFunction(DG0)
+# Courant_num = fd.Function(DG0, name="Courant numerator")
+# #Courant_num_form = dt*(both(un*v_courant)*(fd.dS_v + fd.dS_h)+ un*v_courant*fd.ds_tb )
+# Courant_num_form = dt*(un*v_courant*fd.dx )
+# Courant_denom = fd.Function(DG0, name="Courant denominator")
+# fd.assemble(One*v_courant*fd.dx, tensor=Courant_denom)
+# Courant = fd.Function(DG0, name="Courant")
+# fd.assemble(Courant_num_form, tensor=Courant_num)
+# Courant.interpolate(Courant_num/Courant_denom)
+# print('Courant number', (Courant.dat.data[:]).max())
+
+# print (fd.norm(psi0))   # print the infinity norm of vector x
+# print(np.abs(psi0.dat.data[:]).max())
+# print(np.abs(dt*psi0.dat.data[:]).max())
 
 # #to check CFL number (File :     sw_implicit/straka.py)
 
